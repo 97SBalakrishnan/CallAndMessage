55ebc418a1e2b69b88ec983b8b693e0d7a23091a
97balakrishnan <sbalakrishnan15093@it.ssn.edu.in>
2017-09-05 21:40:25 +0530

    Second commit

--- app/src/main/java/com/example/balakrishnan/callandmessage/MainActivity.java
+++ app/src/main/java/com/example/balakrishnan/callandmessage/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.balakrishnan.callandmessage;
 
+import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
@@ -18,6 +22,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.SEND_SMS}, 1);
+        }
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE}, 1);
+        }
+
+
+
 
         callButton = (Button) findViewById(R.id.buttonCall);
         ePhoneNo = (EditText)findViewById(R.id.phoneNo);
@@ -42,14 +59,19 @@ public class MainActivity extends AppCompatActivity {
                 String sms = eMsg.getText().toString();
 
                 try {
+
+
+
                     SmsManager smsManager = SmsManager.getDefault();
                     smsManager.sendTextMessage(phoneNo, null, sms, null, null);
                     Toast.makeText(getApplicationContext(), "SMS Sent!", Toast.LENGTH_LONG).show();
-                } catch (Exception e) {
-                    Toast.makeText(getApplicationContext(), "SMS faild, please try again later!", Toast.LENGTH_LONG).show();
+                }
+                catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "SMS failed, please try again later!", Toast.LENGTH_LONG).show();
                     e.printStackTrace();
                 }
             }
         });
     }
+   
 }
